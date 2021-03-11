9703287076e98e62ecd269d20fc644c3359783a4
angelIsrael <angeldeux_israel@hotmail.com>
2021-03-11 01:17:14 -0600

    Require

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,15 +1,25 @@
+// en la documentacion de node vienen como importar los require y ejemplos de como utilizarlo
+const fs =  require('fs');
 
+const base = 8;
 console.clear();
 console.log('=====================')
-console.log('     Tabla del 5     ')
+console.log(`     Tabla del ${base}     `)
 console.log('=====================')
 
 
-const base = 5
+let salida = '';
 for (let i = 1; i <= 10; i++ ) {
-        console.log(`${ base } x ${ i } = ${ base * i }`);
+        salida += `${ base } x ${ i } = ${ base * i }\n`;
 }
 
+console.log(salida);
 
+
+// escritura de un archivo
+fs.writeFile( `tabla-${base}.txt`, salida, (err) => {
+        if (err) throw err;
+        console.log(`tabla-${base}.txt creado`);
+} )
 
 
