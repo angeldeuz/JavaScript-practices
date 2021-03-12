47d390858b909dbd0ac1318e860e7b763d266d01
angelIsrael <angeldeux_israel@hotmail.com>
2021-03-12 00:18:19 -0600

    info linea de comando

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,25 +1,16 @@
 // en la documentacion de node vienen como importar los require y ejemplos de como utilizarlo
-const fs =  require('fs');
+const { crearArchivo } = require('./helpers/multiplicar');
 
-const base = 8;
 console.clear();
-console.log('=====================')
-console.log(`     Tabla del ${base}     `)
-console.log('=====================')
 
+const [ , , arg3 = 'base=5'] = process.argv;
+const [ , base] = arg3.split('=');
 
-let salida = '';
-for (let i = 1; i <= 10; i++ ) {
-        salida += `${ base } x ${ i } = ${ base * i }\n`;
-}
+console.log(base);
 
-console.log(salida);
+// const base = 3;
 
-
-// escritura de un archivo
-fs.writeFile( `tabla-${base}.txt`, salida, (err) => {
-        if (err) throw err;
-        console.log(`tabla-${base}.txt creado`);
-} )
-
-
+crearArchivo( base )
+        .then( nombreArchivo => console.log(nombreArchivo, 'creado!') )
+        .catch( err => console.log(err) );
+ 
