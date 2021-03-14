04c0cd04b18d207972a186dc161a06497aae1a3d
Angel Israel Sifuentes <angeldeux_israel@hotmail.com>
2021-03-14 13:03:27 -0600

    App Consola Terminada
    Se termino la app de consola para generar tablas de multiplicar por medio de consola

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,16 +1,27 @@
 // en la documentacion de node vienen como importar los require y ejemplos de como utilizarlo
 const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('./config/yargs');
+
+require('colors');
+
 
 console.clear();
 
-const [ , , arg3 = 'base=5'] = process.argv;
-const [ , base] = arg3.split('=');
+// console.log(process.argv);
+ console.log( argv );
 
-console.log(base);
+crearArchivo( argv.b, argv.l, argv.h )
+    .then( nombreArchivo => console.log(nombreArchivo.rainbow, 'creado!') )
+    .catch( err => console.log(err) );
 
-// const base = 3;
 
-crearArchivo( base )
-        .then( nombreArchivo => console.log(nombreArchivo, 'creado!') )
-        .catch( err => console.log(err) );
+
+//destructuracion de arrays de propiedades del programa -no es practico
+// argv = argumentos 
+// toma los argumentos del los procesos de consola
+// const [ , , arg3 = 'base=5'] = process.argv;
+// const [ , base] = arg3.split('=');
+
+
+        
  
--- 03-bases-node/helpers/multiplicar.js
+++ 03-bases-node/helpers/multiplicar.js
@@ -1,23 +1,26 @@
-
 const fs =  require('fs');
+const colors = require('colors');
 
-const crearArchivo = async( base = 5 ) => {
+const crearArchivo = async( base = 5, listado = false, hasta = 10) => {
 
     try {
-        console.log('=====================');
-        console.log('     Tabla del:', base);
-        console.log('=====================');
-    
-    
         let salida = '';
-        for (let i = 1; i <= 10; i++ ) {
-                salida += `${ base } x ${ i } = ${ base * i }\n`;
+        let  consola = '';
+        for (let i = 1; i <= hasta; i++ ) {
+                salida  += `${ base } x ${ i } = ${ base * i }\n`;
+                consola += `${ base } ${ 'x'.green } ${ i } ${ '='.green } ${ base * i }\n`;
         }
-    
-        console.log(salida);
+
+        if ( listado ) {
+            console.log('====================='.green);
+            console.log('     Tabla del:'.green, colors.red(base));
+            console.log('====================='.green);
+
+            console.log(consola);
+        }
         
         // escritura de un archivo
-        fs.writeFileSync( `tabla-${base}.txt`, salida);
+        fs.writeFileSync( `./salida/tabla-${base}.txt`, salida);
 
         return `tabla-${base}.txt`;
     } catch (error) {
